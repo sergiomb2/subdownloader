3885ed8b92c5b6c90b17b27342acadb0a84b946c
Sérgio M. Basto <sergio@serjux.com>
2021-01-31 01:47:36 +0000

    install also imdbpie

--- scripts/install_python_modules.sh
+++ scripts/install_python_modules.sh
@@ -1,2 +1,2 @@
-pip3 install --user -U langdetect pymediainfo
+pip3 install --user -U langdetect pymediainfo imdbpie
 echo "you may delete this instaltion with rm -rf ~/.local/lib/python3.6/site-packages/langdetect etc "
